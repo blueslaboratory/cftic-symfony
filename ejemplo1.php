a1f74eacd9e928d1e18d8eeb7daeaa69e6c6e5b9
blueslaboratory <myjunkandspam22@gmail.com>
2022-06-20 13:05:04 +0200

    CFTIC - day 0

---
+++ ejemplo1.php
@@ -0,0 +1,25 @@
+<!-- 
+20-06-2022    
+http://localhost:8080/EJEMPLOSPHP/ejemplo1.php
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <?php
+
+        $tipo1 = "Desarrollador de aplicaciones";
+        $tipo2 = "Desarrollador web";
+
+        echo "Trabajos: $tipo1 | " . $tipo2 . "</br>";
+        echo 'Trabajos: $tipo1 | ' . $tipo2 . '</br>';
+    ?>
+
+    
+</body>
+</html>
