cd843b7d678113d4e25ad1dbfaa4ccad6341c8f5
blueslaboratory <myjunkandspam22@gmail.com>
2022-07-06 13:27:42 +0200

    CFTIC - day 12 final

--- EjemplosSymfony/Proyecto4/src/Controller/MostrarController.php
+++ EjemplosSymfony/Proyecto4/src/Controller/MostrarController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Hospital;
+use App\Entity\Dept;
 use App\Repository\HospitalRepository;
+use App\Repository\DeptRepository;
 use Doctrine\ORM\EntityManagerInterface;
-
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +16,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MostrarController extends AbstractController
 {
+    // localhost:8000/verDept
+    #[Route('/verDept', name:'mostrarDept')]
+    public function mostrarDept(Request $request, EntityManagerInterface $em)
+    {
+        $datos = $em->getRepository(Dept::class)->findAll();
+
+        return $this->render('mostrar/verDepartamentos.html.twig', [
+            'datosDepts' => $datos
+        ]);
+    }
+
     // localhost:8000/mostrarHospis
     #[Route('/verHospi', name: 'mostrarHospis')]
     public function mostrarHospi(Request $request, EntityManagerInterface $em)
