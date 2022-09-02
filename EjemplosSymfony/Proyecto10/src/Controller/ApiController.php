0cb591e75d4bfe02da67fd8a50afdd1910539004
blueslaboratory <myjunkandspam22@gmail.com>
2022-09-02 13:20:21 +0200

    CFTIC - day 30 final

--- EjemplosSymfony/Proyecto10/src/Controller/ApiController.php
+++ EjemplosSymfony/Proyecto10/src/Controller/ApiController.php
@@ -12,12 +12,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 // 68 CONSUMO WEB API ACCEDO A DATOS
+// 69 - CONSUMO WEB API STAR WARS
+// 70 - CONSUMO WEB API COMUNIDAD DE MADRID
+// 71 - CONSUMO WEB API PELÍCULAS
 
 class ApiController extends AbstractController
 {
 
     // localhost:8000/consumoapi
-    #[Route('/consumoapi', name:'consumoapi')]
+    #[Route('/consumoapi', name: 'consumoapi')]
     public function consumoapi(Request $request)
     {
         $datos = file_get_contents("https://apiempleadospgs.azurewebsites.net/api/Empleados");
@@ -33,7 +36,7 @@ class ApiController extends AbstractController
 
 
     // localhost:8000/apiStarWars
-    #[Route('/apiStarWars', name:'apiStarWars')]
+    #[Route('/apiStarWars', name: 'apiStarWars')]
     public function apiStarWars(Request $request)
     {
         $datos = file_get_contents("https://swapi.dev/api/people");
@@ -51,7 +54,7 @@ class ApiController extends AbstractController
 
 
     // localhost:8000/apiCentrosDia
-    #[Route('/apiCentrosDia', name:'apiCentrosDia')]
+    #[Route('/apiCentrosDia', name: 'apiCentrosDia')]
     public function apiCentrosDia(Request $request)
     {
         $datos = file_get_contents("https://datos.madrid.es/egob/catalogo/200342-0-centros-dia.json");
@@ -65,6 +68,38 @@ class ApiController extends AbstractController
 
         return $this->render('api/apiCentrosDia.html.twig', [
             'centros' => $centros['@graph']
+        ]);
+    }
+
+
+    // localhost:8000/apiBuscadorPeliculas
+    #[Route('/apiBuscadorPeliculas', name: 'apiBuscadorPeliculas')]
+    public function apiBuscadorPeliculas(Request $request)
+    {
+
+        $titulo = $request->request->get('titulo');
+        $url = 'https://servicioapipeliculas.azurewebsites.net/api/peliculastitulo/';
+        $newUrl = $url . $titulo;
+
+        dump($titulo);
+        dump($newUrl);
+
+        if(empty($titulo)){
+            $newUrl = "https://servicioapipeliculas.azurewebsites.net/api/Peliculas";
+        } 
+
+        dump($newUrl);
+
+        $datos = file_get_contents($newUrl);
+        $peliculas = json_decode($datos, true);
+
+        // Cuando es true, los objects JSON devueltos serán convertidos a array asociativos, 
+        // Cuando es false los objects JSON devueltos serán convertidos a objects.
+
+        dump($peliculas);
+
+        return $this->render('api/apiPeliculas.html.twig', [
+            'peliculas' => $peliculas
         ]);
     }
 }
