05f17e1d7f16c7717ea992e2ffece98772acc9cb
blueslaboratory <myjunkandspam22@gmail.com>
2022-09-07 10:31:55 +0200

    CFTIC - day 33 final

---
+++ VisualStudio 2019/ProyectoWebApi2/ProyectoWebApi2/Controllers/EmpleadosController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// Importar Empleado (importando la carpeta Models), en realidad es un acortamiento
+// acortar la escritura hacia la clase
+using ProyectoWebApi2.Models;
+
+// 07/09/2022
+// Un controlador debe de acabar siempre en Controllers
+
+namespace ProyectoWebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        // Sin el using ProyectoWebApi2.Models; no podemos acortar y seria asi
+        // List<ProyectoWebApi2.Models.Empleado> listaEmpleados = new List<ProyectoWebApi2.Models.Empleado>();
+
+        // Con el acortamiento:
+        List<Empleado> listaEmpleados = new List<Empleado>();
+        
+        // Constructor
+        // Antes de ejecutar cualquier metodo lo primero que hace es pasar por el constructor
+        public EmpleadosController()
+        {
+            // crear el objeto empleado
+            Empleado e1 = new Empleado();
+            e1.id = 1;
+            e1.apellido = "Alberto";
+            e1.oficio = "Programaitor";
+            e1.salario = 12345;
+
+            // otra forma de crear el objeto empleado
+            Empleado e2 = new Empleado { id = 2, apellido = "Floro", oficio = "Sex Symbol", salario = 12345 };
+            Empleado e3 = new Empleado { id = 3, apellido = "Negro", oficio = "Demolisiones", salario = 12345 };
+            Empleado e4 = new Empleado { id = 4, apellido = "Cerezo", oficio = "Analista", salario = 12345 };
+            Empleado e5 = new Empleado { id = 5, apellido = "Salvatore", oficio = "Sistemeitor", salario = 12345 };
+
+            // agregarlo a la lista
+            listaEmpleados.Add(e1);
+            listaEmpleados.Add(e2);
+            listaEmpleados.Add(e3);
+            listaEmpleados.Add(e4);
+            listaEmpleados.Add(e5);
+        }
+
+        // https://localhost:44385/api/Empleados
+        // GET: api/Empleados
+        [HttpGet]
+        public List<Empleado> devolverDatos()
+        {
+            return listaEmpleados;
+        }
+
+        // https://localhost:44385/api/Empleados/2
+        [HttpGet("{id}")]
+        public Empleado devolverEmpleado(int id)
+        {
+            // Esto es lambda, está en C, C++, Java, no se puede programar en prog asincrona sin lambdas
+            // dice que va a subir un documento
+            Empleado e = listaEmpleados.Find(z => z.id == id);
+            return e;
+        }
+    }
+}
---
+++ VisualStudio 2019/ProyectoWebApi2/ProyectoWebApi2/Models/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoWebApi2.Models
+{
+    public class Empleado
+    {
+        // 07/09/2022
+        // shortcut: prop + darle 2 veces al tabulador
+        // string 's' minuscula: tipo de dato
+        // String 'S' mayuscula: clase, permite acceder a metodos
+        
+        public int id { get; set; }
+        public String apellido { get; set; }
+        public String oficio { get; set; }
+        public int salario { get; set; }
+
+    }
+}
